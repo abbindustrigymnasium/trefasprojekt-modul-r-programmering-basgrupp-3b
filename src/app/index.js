6cffb1ad20437248258f34c27473859f97b82b17
Dennis Cejvan <66635133+tramvaj99@users.noreply.github.com>
2024-01-18 21:38:03 +0100

    Make the index page link to the login page

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,14 @@
 import { Text, View } from 'react-native'
 import { Link } from 'expo-router'
+import LoginButton from '../Components/Login'
 
 export default function Page () {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
+        <View className="flex-1 items-center justify-center bg-all-black">
             <View className="">
                 <Text className="">INDEX</Text>
             </View>
-            <Link href="/prefs"> <Text>Go to the 'Preferences' page</Text></Link>
+            <Link href="/login"> <Text>Go to the 'Login' page</Text></Link>
         </View>
     )
 }
