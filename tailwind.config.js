feffd123e3884fa0fa3d4c5866339df0de85743e
Dennis Cejvan <66635133+tramvaj99@users.noreply.github.com>
2024-01-18 21:37:25 +0100

    Add our colors to tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/app/**/*.{js,jsx,ts,tsx}", "./src/Components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+    
+      colors: {
+        'groove-grey': '#B3B3B3',
+        'walter-white': '#FFFFFF',
+        'all-black': '#101010',
+        'less-black': '#1E1E1E',
+        'spott-green': '#1ED760',
+      }
+    },
   },
   plugins: [],
 }
