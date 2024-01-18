eaacae9ce4eb84445c22ed32ac5cbdaa9f04b736
Dennis Cejvan <66635133+tramvaj99@users.noreply.github.com>
2024-01-18 12:57:15 +0100

    Implement tailwind as NativeWind

--- src/app/index.js
+++ src/app/index.js
@@ -1,20 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import { Link } from 'expo-router'
 
 export default function Page () {
     return (
-        <View style={styles.container}>
-            <Text>INDEX</Text>
+        <View className="flex-1 items-center justify-center bg-white">
+            <View className="">
+                <Text className="">INDEX</Text>
+            </View>
             <Link href="/prefs"> <Text>Go to the 'Preferences' page</Text></Link>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
--- src/app/prefs.js
+++ src/app/prefs.js
@@ -1,17 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { Link } from 'expo-router'
-import { Card } from 'react-native-paper'
 
-import ApiCommunicator from '../Components/api_communicator'
+import ApiCommunicator from '../APICommuncations/api_communicator'
 
 export default function Page () {
     return (
         <View style={styles.container}>
             <Text>PREFERENCES</Text>
             <Link href="/"> <Text>Return to home page</Text></Link>
-            <Card>
-                <ApiCommunicator />
-            </Card>
         </View>
     )
 }
