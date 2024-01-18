072c4bb71ba3516cb0647cfc8ff7d1ff78f64fd6
abbolisar <oliver.sarebro@abbgymnasiet.se>
2024-01-18 13:54:04 +0100

    ..

--- src/app/prefs.js
+++ src/app/prefs.js
@@ -1,26 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { Link } from 'expo-router'
+import React, { useState } from 'react'
+import { StyleSheet, Text, SafeAreaView, Pressable } from 'react-native'
 import { Card } from 'react-native-paper'
+import * as Api from './TEMP/for-prefs'
 
-import ApiCommunicator from '../components/api_communicator'
+const [genres, setGenres] = useState([])
+
+function renderArr (arr) {
+    if (arr != []) {
+        return arr.map((item, index) => <Text key={index}>{item}</Text>)
+    }
+}
+
+function resetGenres () {
+    setGenres([])
+}
+
+function fillGenres () {
+    const newGenres = Api.getGenres()
+    setGenres(newGenres)
+}
 
 export default function Page () {
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
             <Text>PREFERENCES</Text>
-            <Link href="/"> <Text>Return to home page</Text></Link>
-            <Card>
-                <ApiCommunicator />
-            </Card>
-        </View>
+            <Pressable style={styles.button} onPress={fillGenres}>
+                <Text>Get all available genres</Text>
+            </Pressable>
+
+            <Card style={styles.list}>{renderArr(genres)}</Card>
+
+            <Pressable style={styles.button} onPress={resetGenres}>
+                <Text>Reset genres</Text>
+            </Pressable>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        marginTop: 120,
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    list: {
+        flex: 1,
+        padding: 2,
+        maxHeight: 120,
+    },
+    button: {
+        flex: 1,
+        padding: 2,
+        backgroundColor: "red",
+        maxHeight: 25
+    }
 })
