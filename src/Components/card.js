6426c7698d053af1165f2a0b386a0ae31488e68c
Dennis Cejvan <66635133+tramvaj99@users.noreply.github.com>
2024-01-22 21:09:12 +0100

    Add swipe action

--- src/Components/card.js
+++ src/Components/card.js
@@ -9,9 +9,90 @@ import {
 } from "react-native";
 import { Image } from "expo-image";
 
-export default function Card(props) {
+import {useWindowDimensions} from 'react-native';
+
+
+
+
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withDecay } from 'react-native-reanimated';
+import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
+
+
+export default function Card({}) {
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+    const rotation = useSharedValue("0deg")
+    const {height, width} = useWindowDimensions();
+
+
+
+    
+const [size, setSize] = React.useState({height: 200, width: 200})
+
+const onLayout=(event)=> {
+  const {x, y, height, width} = event.nativeEvent.layout;
+  setSize({height: height, width: width})
+
+  
+}
+
+    const containerStyle = useAnimatedStyle(() => {
+        return {
+          transform: [
+            {
+              translateX:  translateX.value
+            },
+            {
+              translateY: translateY.value
+            },
+            {
+                rotate: rotation.value
+            }
+          ],
+        };
+      });
+      
+
+    const drag = Gesture.Pan()
+    .onChange((event) => {
+      translateX.value = event.translationX
+      rotation.value = (((translateX.value/(width/2))*6+ "deg"))
+      translateY.value = Math.abs((translateX.value/(width/2))*(height/2))*0.2
+      console.log(translateX.value)
+      console.log(width)
+    
+
+    }).onFinalize((event) => {
+        
+        if(Math.abs(translateX.value)<width*0.7) {
+            translateX.value = withSpring(0)
+            translateY.value = withSpring(0)
+            rotation.value = withSpring("0deg")
+        }else {
+            if(event.velocityX<0.1) {
+                translateX.value = withDecay({
+                    velocity: event.velocityX,
+    
+                   
+                  })
+                translateY.value = withDecay({
+                    velocity: event.velocityY
+    
+                })
+            }else {
+                translateX.value = width
+            }
+            
+            
+        }
+
+    });
+
+
   return (
-    <View className="justify-center bg-less-black flex w-5/6 max-w-[340px] flex-col px-5  py-5" style={{gap: "20px"}}>
+    
+    <GestureDetector gesture={drag}>
+    <Animated.View className={`justify-center bg-less-black flex w-5/6 max-w-[340px] flex-col px-5  py-5`} style={[{gap: "20px"}, containerStyle]}>
       <Image
         loading="lazy"
         source={require("../../assets/lemonade.png")}
@@ -40,7 +121,8 @@ export default function Card(props) {
           className="aspect-square object-contain object-center w-8 overflow-hidden self-center shrink-0 max-w-full"
         />
       </View>
-    </View>
+    </Animated.View>
+    </GestureDetector>
   );
 }
 
--- src/app/recs.js
+++ src/app/recs.js
@@ -1,22 +1,42 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 import { Link } from 'expo-router'
+import * as React from 'react'
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 import Card from '../Components/card'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
+
 
 export default function Page () {
+
+  
+
+
+const [dimensions, setDimensions] = React.useState({height: 200, width: 200})
+
+const onLayout=(event)=> {
+  const {x, y, height, width} = event.nativeEvent.layout;
+  setDimensions({height: height, width: width})
+
+  
+}
+
     return (
-        <SafeAreaView className="flex-1 items-center justify-end bg-all-black pb-6">
+      
+        <GestureHandlerRootView className="flex-1 items-center justify-end bg-all-black pb-6">
           <View className="h-full w-full flex-col items-center justify-center">
-          <Card>
+            <Card 
+            dimensions={dimensions}
+            
+            >
             </Card>
           </View>
-           
             <Pressable
             className=" bg-spott-green w-5/6 max-w-[340px] py-5 flex rounded-2xl items-center justify-center">
   <Text className=" font-semibold text-2xl ">Done</Text>
   </Pressable>
-        </SafeAreaView>
+        </GestureHandlerRootView>
     )
 }
 
