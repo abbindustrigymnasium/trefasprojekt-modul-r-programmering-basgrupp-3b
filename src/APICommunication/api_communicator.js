65f15bd13563ecf026fcaccd08b9c6ea4e582280
Dennis Cejvan <66635133+tramvaj99@users.noreply.github.com>
2024-02-20 20:54:28 +0100

    Comment api_communicator

--- src/APICommunication/api_communicator.js
+++ src/APICommunication/api_communicator.js
@@ -33,6 +33,17 @@ export const spotifyRequest = async ({ endpoint, method = "GET", auth_token, cos
 }
 
 // Custom react hook for Spotify API requests
+
+/**
+ * A custom hook for making requests to the Spotify API.
+ *
+ * @param {string} endpoint - The endpoint to make the request to.
+ * @param {string} method - The HTTP method to use for the request.
+ * @param {Object} costum_headers - Custom headers to include in the request.
+ * @param {Object} body - The request body.
+ * @param {boolean} fetchDirectly - Whether to fetch data directly or not.
+ * @returns {[Object, Function]} - An array containing the response data and the fetch function.
+ */
 export const useSpotifyRequest = ({ endpoint, method, costum_headers, body, fetchDirectly }) => {
   const [data, setData] = useState(null)
   const { session, refreshSession } = useSession()
@@ -42,10 +53,12 @@ export const useSpotifyRequest = ({ endpoint, method, costum_headers, body, fetc
     URL,
     fetchData
   }) => {
+    // If no session token is found, return. This should not happen.
     if (session === null) {
       console.log('No session token found')
       return
     }
+    // If the session token is expired, refresh it and try again.
     if (session.shouldRefresh()) {
       const newSession = await refreshSession()
       const result = await spotifyRequest(
@@ -60,6 +73,7 @@ export const useSpotifyRequest = ({ endpoint, method, costum_headers, body, fetc
       setData(result)
       return result
     } else {
+      // If the session token is valid, make the request.
       const result = await spotifyRequest(
         {
           endpoint: URL ? URL : endpoint,
@@ -69,13 +83,16 @@ export const useSpotifyRequest = ({ endpoint, method, costum_headers, body, fetc
           data: fetchData ? fetchData : body
         }
       )
+      // Set the response data in the state.
       setData(result)
+      // Return the response data, needed when using fetchFromSpotify and not fetching directly.
       return result
     }
 
 
   }
 
+  // If fetchDirectly is true, fetch the data directly.
   useEffect(() => {
     if (fetchDirectly) {
       fetchFromSpotify(endpoint)
